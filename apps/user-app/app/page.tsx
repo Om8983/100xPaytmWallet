cedc7cc137d2d479a11951bdd3eeb4f67d3072e9
Om8983 <omwadhi64@gmail.com>
2025-04-28 21:39:41 +0530

    tailwind config added

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,33 +1,16 @@
-import { RootState } from "@reduxjs/toolkit/query";
-import { prisma } from "@repo/db"
-import { setUserEmail } from "@repo/redux/userSlice";
+"use client"
+import { redirect } from "next/navigation";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+type Prop = {
+  email: string, password: string
+}
 
-
-const ThemeImage = async () => {
-  console.log(process.env.DATABASE_URL)
-  const user = await prisma.user.create({
-    data: {
-      email: "om@gmail.com",
-      password: "12334242423"
-    }
-  })
-
-  // const userDetsails = useSelector((state : any) => state.user)
-
-  // const dispatch = useDispatch();
-  // const handleEmailChange = () => {
-  //   dispatch(setUserEmail({email : "om" , password : "123"}))
-  // }
-
-
+export default function ThemeImage() {
   return (
     <>
-      home
-      {/* <button onClick={handleEmailChange}>Click to change email and password </button> */}
+      ThemeImage
     </>
-  );
+  )
 };
-
-export default ThemeImage;
