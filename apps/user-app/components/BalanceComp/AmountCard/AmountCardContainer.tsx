76194bc5e46ca4fcdd8835f2aac5461130ac73ca
Om8983 <omwadhi64@gmail.com>
2026-03-21 23:51:54 +0530

    added border animation for balance cards

--- apps/user-app/components/BalanceComp/AmountCard/AmountCardContainer.tsx
+++ apps/user-app/components/BalanceComp/AmountCard/AmountCardContainer.tsx
@@ -1,21 +1,56 @@
 "use client";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 type Props = {
     children: React.ReactNode,
-    containerStyles?: string,
-    cardStyles?: string
+    id: string;
+    className?: string;
+    cardRefs: React.MutableRefObject<Record<string, HTMLDivElement | null>>;
+    isWrapperHovered: boolean;
+    glowPositions: Record<string, { x: number; y: number }>;
 }
-export const AmountCardContainer = ({  children, cardStyles, containerStyles }: Props) => {
+
+
+export const AmountCardContainer = ({ children, id, className, cardRefs, isWrapperHovered, glowPositions }: Props) => {
+
+    // these are the respective x and y positions of the mouse in teh wrapper.
+    const glow = glowPositions[id]
+
     return (
-        <motion.div
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, ease: "easeInOut" }}
-            className={`border-none relative flex items-center justify-center ${containerStyles}`}>
-            <div className={`absolute rounded-2xl lg:w-[400px] lg:h-[200px] border-[1px] z-0 `}></div>
-            <div className={`${cardStyles} relative  z-10 sm:w-[100px] sm:h-[60px] lg:w-[380px] lg:h-[180px] rounded-[calc(1rem-0.4rem)] border-none outline-none shadow-md p-5`}>
-                {children}
-            </div>
-        </motion.div>
+        <>
+            <motion.div
+                key={id}
+                ref={el => { cardRefs.current[id] = el }}
+                className="relative lg:w-[400px] lg:h-[200px] sm:w-[100px] sm:h-[60px] rounded-2xl"
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, ease: "easeInOut" }}
+            >
+                {/* this will be glowed when hovered. */}
+                <motion.div
+                    className="absolute inset-0 rounded-2xl"
+                    animate={{ opacity: isWrapperHovered && glow ? 1 : 0 }}
+                    transition={{ duration: 0.5 }}
+                    style={{
+                        background: glow ? `radial-gradient(300px circle at ${glow.x}px ${glow.y}px, rgba(147, 51, 234, 0.8), rgba(236, 72, 153, 0.4) 40%, transparent 70%)` : "none",
+                        zIndex: 0,
+                    }}
+                />
+
+                <div
+                    className={` absolute rounded-[calc(1rem-2px)] shadow-md p-5 overflow-hidden z-1 inset-[0.18rem] ${className} `}
+                >
+                    {children}
+                    <motion.div
+                        className="absolute inset-0 pointer-events-none"
+                        animate={{ opacity: isWrapperHovered && glow ? 1 : 0 }}
+                        transition={{ duration: 0.5 }}
+                    // style={{
+                    //     // background: glow ? `radial-gradient(250px circle at ${glow.x}px ${glow.y}px, rgba(255,255,255,0.07), transparent 60%)` : "none",
+                    //     // zIndex: 2,
+                    // }}
+                    />
+                </div>
+            </motion.div>
+        </>
     )
 }
--- apps/user-app/components/BalanceComp/AmountCard/AmountCards.tsx
+++ apps/user-app/components/BalanceComp/AmountCard/AmountCards.tsx
@@ -1,25 +1,58 @@
 "use client";
-import React, { useState } from 'react'
-import { AmountCardContainer } from './AmountCardContainer'
+import React, { useState, useRef } from 'react'
+import { motion } from 'motion/react'
 import { AmountCardContent } from './AmountCardContent'
 import { AmountCardTitle } from './AmountCardTitle'
 import { Button } from '@repo/ui/button'
 import { IconCircleCheckFilled } from '@tabler/icons-react'
+import { AmountCardContainer } from './AmountCardContainer';
+
 
 type AmountCardProps = {
     totalBalance?: number;
 }
+type CardGlow = {
+    x: number;
+    y: number;
+}
 export const AmountCards = ({ totalBalance }: AmountCardProps) => {
     const [loading, setLoading] = useState(false)
+
+    const [isWrapperHovered, setIsWrapperHovered] = useState(false)
+    const [glowPositions, setGlowPositions] = useState<Record<string, CardGlow>>({})
+    const cardRefs = useRef<Record<string, HTMLDivElement | null>>({})
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        const updated: Record<string, CardGlow> = {}
+        // here we are running a loop to assign the mouse position i.e x and y, to each card.That way each card even when not hovered will get the positions of the mouse
+        for (const [id, el] of Object.entries(cardRefs.current)) {
+            if (!el) continue
+            const rect = el.getBoundingClientRect()
+            // here to mention we are only targeting the parent wrapper. That means we want the coordinates of the mouse only when mouse enters the vicinity of the parent element. Thats the reason of subtracting rect.left and rect.top to get the positions of mouse from the top and left of the container wrapper
+            updated[id] = {
+                x: e.clientX - rect.left,
+                y: e.clientY - rect.top,
+            }
+        }
+        // finally we just set the location of mouse with respect to each card.
+        setGlowPositions(updated)
+    }
+
     return (
-        <div className=" flex gap-2 items-center mb-3 ">
-            <AmountCardContainer containerStyles="ml-2" cardStyles="bg-[#393bfe]"  >
-                {/* i wanna have a encode number representation animation. Like when viewAmount is being clicked then random nos. should be animated and then real number will be presented. */}
+        <div
+            // mouse move given to this because whenever the mouse will be hovered of enter the vicinity of this parent class of the cards then mouse coordination will be shared to the inidividual card.
+            onMouseMove={handleMouseMove}
+            onMouseEnter={() => setIsWrapperHovered(true)}
+            onMouseLeave={() => setIsWrapperHovered(false)}
+            className="flex gap-2 items-center mb-3 "
+            
+        >
+            <AmountCardContainer id='income' className='bg-[#393bfe]' isWrapperHovered={isWrapperHovered} cardRefs={cardRefs} glowPositions={glowPositions}>
                 <AmountCardTitle showIcon={true} title="TOTAL INCOME" iconStyle="stroke-[#393bfe]" classname="text-white" />
-                <AmountCardContent classname="text-white" iconStyle="stroke-white" icon={false} >
+                <AmountCardContent classname="text-white" iconStyle="stroke-white" icon={false}>
                     <Button
                         text="Check Balance"
-                        className="text-[0.78rem] w-auto px-2 py-1 rounded-xl text-nowrap bg-white cursor-pointer "
+                        className="text-[0.78rem] w-auto px-2 py-1 rounded-xl text-nowrap bg-white cursor-pointer"
                         isLogin={false}
                         icon={<IconCircleCheckFilled size={18} color='green' />}
                         handleClick={() => { }}
@@ -27,15 +60,14 @@ export const AmountCards = ({ totalBalance }: AmountCardProps) => {
                     />
                 </AmountCardContent>
             </AmountCardContainer>
-            <AmountCardContainer containerStyles="ml-5" cardStyles="bg-white" >
+            <AmountCardContainer id='spendings' className='bg-white' isWrapperHovered={isWrapperHovered} cardRefs={cardRefs} glowPositions={glowPositions}>
                 <AmountCardTitle showIcon={false} title="SPENDINGS" />
                 <AmountCardContent classname="text-white" />
             </AmountCardContainer>
-            <AmountCardContainer containerStyles="ml-5" cardStyles="bg-white"  >
+            <AmountCardContainer id='received' className='bg-white' isWrapperHovered={isWrapperHovered} cardRefs={cardRefs} glowPositions={glowPositions}>
                 <AmountCardTitle showIcon={false} title="RECEIVED" />
                 <AmountCardContent classname="text-white" />
             </AmountCardContainer>
-
         </div>
     )
 }
